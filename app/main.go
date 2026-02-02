6cd279dfc2733ec5f2f13dd0c9d2c3dc92e66582
Aayush Khanna <ayush25khanna@gmail.com>
2026-02-02 18:29:25 +0530

    feat: add pprof

--- app/main.go
+++ app/main.go
@@ -7,6 +7,8 @@ import (
 	"io"
 	"log"
 	"net"
+	"net/http"
+	_ "net/http/pprof"
 	"os"
 
 	"github.com/codecrafters-io/redis-starter-go/internal/commands"
@@ -19,21 +21,27 @@ var debug = flag.Bool("debug", false, "Enable debug mode to log all commands")
 
 func handleConn(c net.Conn) {
 	defer c.Close()
+
 	reader := bufio.NewReader(c)
 	writer := bufio.NewWriter(c)
 	defer writer.Flush()
-	Conn := pubsub.Connection{W: writer, Channels: make(map[string]struct{})}
+
+	Conn := pubsub.Connection{
+		W:        writer,
+		Channels: make(map[string]struct{}),
+	}
+
 	for {
 		msg, err := parser.Parse(reader)
 		if err != nil {
 			if err == io.EOF {
 				return
 			}
-			// Send error response to client and close connection
 			writer.Write([]byte(fmt.Sprintf("-ERR %s\r\n", err.Error())))
 			writer.Flush()
 			return
 		}
+
 		commands.ExecuteCommands(msg, &Conn)
 		writer.Flush()
 	}
@@ -43,26 +51,33 @@ func main() {
 	flag.Parse()
 	commands.DebugMode = *debug
 
-	// Bind to port 6379
+	go func() {
+		log.Println("pprof listening on http://localhost:6060")
+		log.Println(http.ListenAndServe("localhost:6060", nil))
+	}()
+
 	log.Printf("STARTING REDIS SERVER")
 	if commands.DebugMode {
 		log.Printf("DEBUG MODE ENABLED")
 	}
+
 	l, err := net.Listen("tcp", "0.0.0.0:6379")
 	if err != nil {
 		log.Println("Failed to bind to port 6379")
 		os.Exit(1)
 	}
 	defer l.Close()
+
 	utils.GlobalInitFunction()
+
 	for {
 		conn, err := l.Accept()
-		log.Printf("RECIEVED A CONNECTION")
+		log.Printf("RECEIVED A CONNECTION")
 		if err != nil {
-			log.Println("Error accepting connection: ", err.Error())
+			log.Println("Error accepting connection:", err)
 			os.Exit(1)
 		}
-		// TODO: update this to an event loop type setup/threadpool
+
 		go handleConn(conn)
 	}
 }
